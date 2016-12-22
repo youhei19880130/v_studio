3619657739f5c2a6ac5aedca195617d0d3ea9117
yohhei someya <youhei19880130@yahoo.co.jp>
2016-12-23 02:02:42 +0900

    Add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :studios
   resources :accounts
   resources :searches, only: :index
+  resources :admin, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
