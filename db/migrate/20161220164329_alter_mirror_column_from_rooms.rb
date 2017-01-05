25a98b864df5fbd9aceb9d4f1184f5a2c9898325
yohhei someya <youhei19880130@yahoo.co.jp>
2017-01-06 02:48:22 +0900

    Remove unnecessary migration file

--- db/migrate/20161220164329_alter_mirror_column_from_rooms.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-class AlterMirrorColumnFromRooms < ActiveRecord::Migration[5.0]
-  def change
-    change_column :rooms, :mirror, :boolean, null: false, default: false
-  end
-end
