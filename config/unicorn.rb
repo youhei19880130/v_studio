9f7007aa2f32c118a04531f78f03d807676ca56f
yohhei someya <youhei19880130@yahoo.co.jp>
2017-01-14 00:44:30 +0900

    Refactor provisioning

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,31 +1,28 @@
-# -*- coding: utf-8 -*-
-worker_processes 2
-
-app_path = '/var/www/app/v_studio'
-app_shared_path = "#{app_path}/shared"
-listen = "#{app_shared_path}/tmp/unicorn.sock"
-pid = "#{app_shared_path}/tmp/unicorn.pid"
-
-log = Dir.pwd + '/log/unicorn.log'
-stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
-
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+listen '/var/www/app/v_studio/tmp/unicorn.sock'
+pid    '/var/www/app/v_studio/tmp/unicorn.pid'
 
 before_fork do |server, worker|
-defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-
-old_pid = "#{ server.config[:pid] }.oldbin"
-unless old_pid == server.pid
-  begin
-    sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-    Process.kill :QUIT, File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
-end
-end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end 
 
 after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
+
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
