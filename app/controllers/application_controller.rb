b4b96bd46880f190d9aa55759e5a8b998036db2d
yohhei someya <youhei19880130@yahoo.co.jp>
2017-03-19 01:40:07 +0900

    Undisplay query string at studio#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_search_info
+
+  private
+
+  def set_search_info
+    session[:area] = params[:area] if params[:area]
+    session[:people] = params[:people] if params[:people]
+  end
 end
