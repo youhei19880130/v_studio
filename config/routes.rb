f3ce0077745c62dcaef4093b0cdebb816780a19e
yohhei someya <youhei19880130@yahoo.co.jp>
2017-05-03 16:05:04 +0900

    Redirect with uncatcatchable url

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
     resources :rooms
     resources :areas
   end
+  get '*unmatched_route', to: redirect('/'), format: false unless Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
