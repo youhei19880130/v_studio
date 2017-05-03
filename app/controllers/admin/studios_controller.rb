e83659de8d957aca963258259c7fa103c201bb2e
yohhei someya <youhei19880130@yahoo.co.jp>
2017-05-03 11:24:19 +0900

    Hotfix admin::studio

--- app/controllers/admin/studios_controller.rb
+++ app/controllers/admin/studios_controller.rb
@@ -71,7 +71,7 @@ module Admin
 
     # Use callbacks to share common setup or constraints between actions.
     def set_studio
-      @studio = Studio.find(params[:id])
+      @studio = Studio.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
