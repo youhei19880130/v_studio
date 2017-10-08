7753e569cb222f1a6299dfbe305f5629933b93f6
yohhei someya <youhei19880130@yahoo.co.jp>
2017-10-09 06:58:39 +0900

    Add basic auth at staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_search_info
+  before_action :basic_auth, if: -> { Rails.env.staging? }
 
   private
 
   def set_search_info
     session[:area] = params[:area] if params[:area]
     session[:people] = params[:people] if params[:people]
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == 'sagasu' && pass == 'space'
+    end
   end
 end
