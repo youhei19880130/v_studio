f4582a601ffc02ecee13f2af7f24ca52824d9166
yohhei someya <youhei19880130@yahoo.co.jp>
2017-10-26 22:36:26 +0900

    Configure production deploy setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,13 +1,13 @@
-# frozen_string_literal: true
-set :stage, :production
-role :app, %w(root@160.16.111.233)
-role :web, %w(root@160.16.111.233)
-role :db, %w(root@160.16.111.233)
-
+set :pty, true
 server '160.16.111.233',
-       user: 'root',
-       roles: %w(web app db),
-       ssh_options: {
-         auth_methods: %w(password),
-         password: 'sagasu.space'
-       }
+  user: 'root',
+  roles: %w{app db web},
+  ssh_options: {
+   auth_methods: %w(password),
+   password: 'sagasu.space'
+  }
+
+set :linked_dirs, %w{bin log tmp/backup tmp/pids tmp/sockets vendor/bundle}
+
+set :unicorn_pid, "/var/www/app/v_studio/shared/tmp/pids/unicorn.pid"
+set :unicorn_exec, -> { "unicorn_rails" }
