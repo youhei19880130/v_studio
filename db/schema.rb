9f374345231400b5e45703bcfdf4c57be2208ce4
ryom-a <ryoma.shikami@gmail.com>
2018-11-14 18:15:35 +0900

    Delete Image Column from Studios Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180927083453) do
+ActiveRecord::Schema.define(version: 20181114085207) do
 
   create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -78,22 +78,21 @@ ActiveRecord::Schema.define(version: 20180927083453) do
     t.string   "tel"
     t.time     "start_hours"
     t.time     "end_hours"
-    t.boolean  "late_night",                          default: false
-    t.boolean  "locker_room",                         default: false
-    t.boolean  "parking",                             default: false
+    t.boolean  "late_night",                       default: false
+    t.boolean  "locker_room",                      default: false
+    t.boolean  "parking",                          default: false
     t.string   "cancell_deadline"
-    t.binary   "image",              limit: 16777215
     t.string   "url"
     t.text     "feature",            limit: 65535
     t.text     "remarks",            limit: 65535
     t.text     "memo",               limit: 65535
-    t.integer  "status",             limit: 1,        default: 0
+    t.integer  "status",             limit: 1,     default: 0
     t.string   "slug"
     t.string   "meta_title"
     t.string   "meta_description"
     t.string   "meta_ogp_image_url"
-    t.datetime "created_at",                                          null: false
-    t.datetime "updated_at",                                          null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
     t.text     "latlng",             limit: 65535
     t.index ["slug"], name: "index_studios_on_slug", unique: true, using: :btree
   end
