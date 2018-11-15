b49a34c934e43f6ce48dd820312dc43615a19312
yohhei someya <youhei19880130@yahoo.co.jp>
2018-11-15 14:18:09 +0900

    Arrange indent

--- db/schema.rb
+++ db/schema.rb
@@ -78,21 +78,21 @@ ActiveRecord::Schema.define(version: 20181114085207) do
     t.string   "tel"
     t.time     "start_hours"
     t.time     "end_hours"
-    t.boolean  "late_night",                    default: false
-    t.boolean  "locker_room",                   default: false
-    t.boolean  "parking",                       default: false
+    t.boolean  "late_night",                          default: false
+    t.boolean  "locker_room",                         default: false
+    t.boolean  "parking",                             default: false
     t.string   "cancell_deadline"
     t.string   "url"
     t.text     "feature",            limit: 65535
     t.text     "remarks",            limit: 65535
     t.text     "memo",               limit: 65535
-    t.integer  "status",             limit: 1,  default: 0
+    t.integer  "status",             limit: 1,        default: 0
     t.string   "slug"
     t.string   "meta_title"
     t.string   "meta_description"
     t.string   "meta_ogp_image_url"
-    t.datetime "created_at",                                    null: false
-    t.datetime "updated_at",                                    null: false
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
     t.text     "latlng",             limit: 65535
     t.index ["slug"], name: "index_studios_on_slug", unique: true, using: :btree
   end
