36b266115ea006008dc003efa865c7beded86fc4
ryom-a <ryoma.shikami@gmail.com>
2018-11-19 13:51:44 +0900

    Modify validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 class Room < ApplicationRecord
   belongs_to :studio
+  validates :capacity, presence: true, numericality: { only_integer: true, grater_than: 0 }
 
   scope :displayed, -> { where(status: Room.statuses[:active]) }
   scope :by_studio, ->(studio_id) { where(studio_id: studio_id) if studio_id.present?}
 
   enum status: { inactive: 0, active: 1 }
   enum floor: { 'フローリング': 1, 'リノリウム': 2, 'その他': 99 }
-
-  validates :capacity, presence: true
 end
