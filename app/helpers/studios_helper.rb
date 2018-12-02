126e1eb2755ad2c66085fb5fab67208af4f0c09a
ryom-a <ryoma.shikami@gmail.com>
2018-12-02 17:24:48 +0900

    Add people_range_format

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -11,4 +11,19 @@ module StudiosHelper
       '×'
     end
   end
+
+  def people_range_format(people_range_id)
+    logger.info("people_range_id: #{people_range_id}")
+    return '' if people_range_id.blank?
+    min = PeopleRange.find(people_range_id).min
+    max = PeopleRange.find(people_range_id).max
+
+    if people_range_id.to_i == Settings.people_range.all
+      ''
+    elsif max == PeopleRange.maximum(:max)
+      "#{min}-"
+    else
+      "#{min}-#{max}"
+    end
+  end
 end
