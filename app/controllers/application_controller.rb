ba8b9f6d12c0f13fd43fe78ecb09912e8b1682be
ryom-a <ryoma.shikami@gmail.com>
2018-12-12 09:50:57 +0900

    Modify definition of session[:area]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,9 @@ class ApplicationController < ActionController::Base
   private
 
   def set_search_info
-    session[:area] = if params[:area].present? && params[:area].to_i == 0
-                       params[:area]
-                     elsif params[:area].to_i > 0
-                       Area.find(params[:area].to_i).slug
-                     end
+    if params[:area].present? && params[:area].to_i == 0
+      session[:area] = params[:area]
+    end
     if params[:people].present? && !params[:people].include?("-")
       session[:people] = params[:people]
     end
