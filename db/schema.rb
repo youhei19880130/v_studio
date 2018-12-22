df4089bfa549aebdffb7b1958706bd1ce611ea8a
ryom-a <ryoma.shikami@gmail.com>
2018-12-22 14:39:04 +0900

    Delete drop_table migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181220075815) do
+ActiveRecord::Schema.define(version: 20181202084316) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "prefecture"
