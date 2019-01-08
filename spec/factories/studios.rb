f169bf42a206adcaeef021f53f079a6b371f1d6b
ryom-a <ryoma.shikami@gmail.com>
2019-01-08 16:18:29 +0900

    Rewrite factory with ffaker

--- spec/factories/studios.rb
+++ spec/factories/studios.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :studio do
-    name 'SampleStudio'
-    area_id 2
-    address '東京都渋谷区渋谷1-1-1'
-    nearest_station_1 '渋谷'
-    tel '03-1111-1111'
+    name { FFaker::Food.fruit + " Studio" }
+    area_id { (1..28).to_a.push(99)[rand(29)] }
+    address { FFaker::AddressJA.address }
+    nearest_station_1 { FFaker::AddressJA.ward }
+    tel { FFaker::PhoneNumber.short_phone_number }
     start_hours '09:00:00'
     end_hours '23:00:00'
-    late_night 1
-    locker_room 1
-    parking 0
-    cancell_deadline '時間貸しの場合 1週間前までは無料 2日～6日前までは50% 前日・当日は100% 一日貸しの場合 2日前までは50% 前日・当日は100%'
-    url 'http://sample_studio.com'
+    late_night { Random.rand(0..1) }
+    locker_room { Random.rand(0..1) }
+    parking { Random.rand(0..1) }
+    cancell_deadline { Random.rand(1..14).to_s + "日前まで" }
+    url { FFaker::Internet.http_url }
     feature 'sample'
     status 1
-    slug 'sample'
+    slug { "#{name}".downcase }
   end
 end
