d41c8df63de2084aeda14999dca30b41a8679ee7
yohhei someya <youhei19880130@yahoo.co.jp>
2019-04-23 23:45:11 +0900

    Modify room infos display

--- app/helpers/studios_helper.rb
+++ app/helpers/studios_helper.rb
@@ -5,10 +5,15 @@ module StudiosHelper
   end
 
   def table_format(value)
-    if value == true || value.present?
-      '◯'
-    else
+    return '○' if value.is_a?(String)
+
+    case value
+    when Settings.status.none
       '×'
+    when Settings.status.exist
+      '○'
+    when Settings.status.unknown
+      '不明'
     end
   end
 
